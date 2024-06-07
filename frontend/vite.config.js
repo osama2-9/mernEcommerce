ee9d7da438c11f106d125f8c4a9d02310d72b961
Osama <97450706+osama2-9@users.noreply.github.com>
2024-06-08 02:47:02 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000/",
+        target: "https://onlineshop-umber.vercel.app/",
         secure: false,
         changeOrigin: true,
       },
