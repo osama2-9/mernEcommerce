a12c3b4df585976ec9637b204eb2da2d1ec16109
Osama <97450706+osama2-9@users.noreply.github.com>
2024-06-10 00:40:03 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4000/",
+        target: "https://mernecommerce-fsea.onrender.com/",
         secure: false,
         changeOrigin: true,
       },
