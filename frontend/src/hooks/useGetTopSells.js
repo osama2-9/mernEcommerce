7e4e0636c458b8bbce3fbd8013fd65c0463233d9
Osama <osamaalsrraj3@gmail.com>
2024-06-09 23:40:51 +0300

    .

--- frontend/src/hooks/useGetTopSells.js
+++ frontend/src/hooks/useGetTopSells.js
@@ -5,13 +5,8 @@ const useGetTopSells = () => {
 
   const getTopSellProducts = async () => {
     try {
-<<<<<<< HEAD
       const res = await fetch("/api/order/topSell");
-=======
-      const res = await fetch(
-        "/api/product/topSell"
-      );
->>>>>>> bca60c26b866647726220ede969a0e6a28b11822
+
       const data = await res.json();
       if (data.error) {
         toast.error(data.error);
