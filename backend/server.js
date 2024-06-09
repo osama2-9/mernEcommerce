8cadfd23d7f5783e47277ceda36e27135c0c805f
Osama <osamaalsrraj3@gmail.com>
2024-06-10 00:12:09 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
+app.use("/", (req, res) => {
   res.json({
     message: "API Work",
   });
