a568f5b9308251ad7b7dfe90361eb34693e0d4b0
Osama <osamaalsrraj3@gmail.com>
2024-06-09 23:46:24 +0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-<<<<<<< HEAD
         target: "http://localhost:4000/",
-=======
-        target: "https://onlineshopping-ruddy.vercel.app",
->>>>>>> bca60c26b866647726220ede969a0e6a28b11822
         secure: false,
         changeOrigin: true,
       },
