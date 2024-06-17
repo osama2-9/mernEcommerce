b3804cb479b0b04daf92278d808ff095d75bd246
Osama <osamaalsrraj3@gmail.com>
2024-06-17 15:29:44 +0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,13 @@ import mongoose from "mongoose";
 const dbConnect = async () => {
   mongoose
     .connect(
-      "mongodb+srv://osamaalsrraj3:osama@e-commerce.qrqncvk.mongodb.net/"
+      "mongodb+srv://osamaalsrraj3:osama@e-commerce.qrqncvk.mongodb.net/",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 15000,
+        socketTimeoutMS: 45000,
+      }
     )
     .then(() => {
       console.log("connect");
@@ -11,6 +17,7 @@ const dbConnect = async () => {
     .catch((err) => {
       console.log(err);
     });
+    mongoose.set('debug', true);
 };
 
 export { dbConnect };
